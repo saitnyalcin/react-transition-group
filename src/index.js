90432d8e76d148e99f706cb29da931740b4fc100
steveyalcin <steven.yalcin@gmail.com>
2020-04-05 12:15:55 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import './style.css';
+import Nav from './Nav';
+import Home from './Home';
+import About from './About';
+
+// Does the user's browser support the HTML5 history API?
+// If the user's browser doesn't support the HTML5 history API then we
+// will force full page refreshes on each page change.
+const supportsHistory = 'pushState' in window.history;
+
+const App = () => (
+  <BrowserRouter forceRefresh={!supportsHistory}>
+    <div>
+      <Nav />
+      <main>
+        <Route
+          render={({ location }) => {
+            const { pathname } = location;
+            return (
+              <TransitionGroup>
+                <CSSTransition
+                  key={pathname}
+                  classNames="page"
+                  timeout={{
+                    enter: 1000,
+                    exit: 1000
+                  }}
+                >
+                  <Route
+                    location={location}
+                    render={() => (
+                      <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/about" component={About} />
+                      </Switch>
+                    )}
+                  />
+                </CSSTransition>
+              </TransitionGroup>
+            );
+          }}
+        />
+      </main>
+    </div>
+  </BrowserRouter>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<App />, document.getElementById('root'));
